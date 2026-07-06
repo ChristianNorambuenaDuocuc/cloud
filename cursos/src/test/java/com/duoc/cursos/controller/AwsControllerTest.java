92ea234d202a23a698832f791ca6569aba748acc
ChristianNorambuenaDuocuc <chri.norambuena@duocuc.cl>
2026-07-06 19:37:17 -0400

    Create AwsControllerTest.java

---
+++ cursos/src/test/java/com/duoc/cursos/controller/AwsControllerTest.java
@@ -0,0 +1,35 @@
+package com.duoc.cursos.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class AwsControllerTest {
+    @Test
+    void testDeleteFile() {
+
+    }
+
+    @Test
+    void testDownloadS3File() {
+
+    }
+
+    @Test
+    void testGetS3FileContent() {
+
+    }
+
+    @Test
+    void testGetS3Files() {
+
+    }
+
+    @Test
+    void testMoveFile() {
+
+    }
+
+    @Test
+    void testUploadFile() {
+
+    }
+}
